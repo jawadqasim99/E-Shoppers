7c3de8a131cca3ee1a877de95edc33fdbc57e8fa
jawad qasim <jawadqasim99@gmail.com>
2024-02-07 12:28:09 +0500

    context

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product  from './pages/Product';
 import ShopCategory from './pages/ShopCategory';
 import Shop from './pages/Shop';
 import Cart from './pages/Cart';
+import men_banners from '../src/components/assets/banner_mens.png';
+import womem_benners from '../src/components/assets/banner_women.png';
+import kids_banners from '../src/components/assets/banner_kids.png';
 
 
 
@@ -17,9 +20,9 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path='/' element={<Shop/>}/>
-      <Route path='/mens' element={<ShopCategory category="men"/> }/>
-      <Route path='/womens' element={<ShopCategory category="women"/> }/>
-      <Route path='/Kids' element={<ShopCategory category="kid"/> }/>
+      <Route path='/mens' element={<ShopCategory banner={men_banners} category="men"/> }/>
+      <Route path='/womens' element={<ShopCategory banner={womem_benners} category="women"/> }/>
+      <Route path='/Kids' element={<ShopCategory banner={kids_banners} category="kid"/> }/>
       <Route path='/product' element={<Product/>}>
         <Route path=':productId' element={<Product/>}/>
       </Route>
